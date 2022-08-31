751ad18172a0a58a9fedb2483a364bf95ce3893f
Bhargav Reddy <blank2@unh.newhaven.edu>
2022-08-30 23:47:14 -0400

    Python Assignment week 1

---
+++ helloworld.py
@@ -0,0 +1,10 @@
+Python 3.10.6 (tags/v3.10.6:9c7b4bd, Aug  1 2022, 21:53:49) [MSC v.1932 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+# Program1
+# Bhargav Reddy Lankireddy
+# Id: 00798146
+# Hello World Program
+# Versioning : Link Here
+
+print("Hello World")
+Hello World
---
+++ module_hello.py
@@ -0,0 +1,9 @@
+# Week 1
+
+# BhargavReddy Lankireddy 00798146
+
+# Executing "Hello World" Program
+
+#Version control : 
+
+print("Hello World")
